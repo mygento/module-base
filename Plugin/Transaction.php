141c90643442dcf62c08c01debe5ce3d4524369f
nikita tarasov <nikita@mygento.ru>
2018-12-05 14:39:07 +0300

    plugins

---
+++ Plugin/Transaction.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2014-2018 Mygento (https://www.mygento.ru)
+ * @package Mygento_Base
+ */
+
+namespace Mygento\Base\Plugin;
+
+class Transaction
+{
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     *
+     * @param \Magento\Sales\Model\Order\Payment\Transaction $subject
+     * @param mixed $result
+     */
+    public function afterGetTransactionTypes(
+        \Magento\Sales\Model\Order\Payment\Transaction $subject,
+        $result
+    ) {
+        return array_merge($result, [
+            'fiscal_receipt' => __('Fiscal receipt'),
+            'fiscal_refund' => __('Fiscal receipt refund'),
+        ]);
+    }
+}
