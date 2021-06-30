6c0edeef5b8d858a2c4bfb2a3776b97360307b62
R.Mavlyanov <r.mavlyanov@mygento.ru>
2021-06-30 14:21:29 +0300

    [LIS-16] Add test for SkippedItemFixer

---
+++ Test/Unit/SkippedItemFixerTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2014-2021 Mygento (https://www.mygento.ru)
+ * @package Mygento_Base
+ */
+
+namespace Mygento\Base\Test\Unit;
+
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Mygento\Base\Model\Recalculator\ResultFactory;
+use Mygento\Base\Test\Extra\DiscountHelperInterfaceFactory;
+use Mygento\Base\Test\Extra\ExpectedMaker;
+use PHPUnit\Framework\TestCase;
+
+class SkippedItemFixerTest extends TestCase
+{
+    /**
+     * @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
+     */
+    private $objectMan;
+
+    /**
+     * @dataProvider \Mygento\Base\Test\Unit\SkippedItemFixerDataProvider::dataProviderItemsToSkip
+     * @param mixed $item
+     * @param mixed $expected
+     * @throws \Exception
+     */
+    public function testCalculation($item, $expected)
+    {
+        $fixer = $this->getFixerInstance();
+
+        $itemResult = $fixer->execute($item);
+
+        if (!$expected) {
+            ExpectedMaker::dump($itemResult);
+        }
+
+        self::assertEquals($expected['sum'], $itemResult->getSum(), 'Sum failed');
+        self::assertEquals($expected['price'], $itemResult->getPrice(), 'Price failed');
+    }
+
+    /**
+     * @return \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
+     */
+    public function getObjectManager(): ObjectManager
+    {
+        if (!$this->objectMan) {
+            $this->objectMan = new ObjectManager(
+                $this
+            );
+        }
+
+        return $this->objectMan;
+    }
+
+    /**
+     * @return \Mygento\Base\Service\PreHandlers\SkipItems\SkippedItemFixer
+     */
+    private function getFixerInstance()
+    {
+        /** @var \Mygento\Base\Test\Extra\DiscountHelperInterfaceFactory $discountHelperFactory */
+        $discountHelperFactory = $this->getObjectManager()->getObject(
+            DiscountHelperInterfaceFactory::class,
+            ['objectManager' => $this->objectMan]
+        );
+
+        $resultFactory = $this->getRecalculateResultFactory();
+
+        return $this->getObjectManager()->getObject(
+            \Mygento\Base\Service\PreHandlers\SkipItems\SkippedItemFixer::class,
+            [
+                'discountHelperFactory' => $discountHelperFactory,
+                'recalculateResultFactory' => $resultFactory,
+            ]
+        );
+    }
+
+    /**
+     * @return ResultFactory
+     */
+    private function getRecalculateResultFactory(): ResultFactory
+    {
+        /** @var \Mygento\Base\Test\Extra\GetRecalculateResultFactory $recalculateResultFactory */
+        $recalculateResultFactory = $this->getObjectManager()->getObject(
+            \Mygento\Base\Test\Extra\GetRecalculateResultFactory::class
+        );
+
+        return $recalculateResultFactory->get($this);
+    }
+}
